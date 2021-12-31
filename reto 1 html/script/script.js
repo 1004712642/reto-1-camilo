bb5039011b91bd327a8018857a47113bf88444e4
camilo castillo <92165505+1004712642@users.noreply.github.com>
2021-12-31 12:45:00 -0500

    Add files via upload
    lo ise de una manera muy autonoma

---
+++ reto 1 html/script/script.js
@@ -0,0 +1,33 @@
+ 
+function capturar(){
+    console.log("capturar");
+    function moneda(dolar, pesomexicano, pesocolombiano, euro, libraesterlina) {
+        this.dolar=dolar
+        this.pesomexicano=pesomexicano
+        this.pesocolombiano=pesocolombiano
+        this.euro=euro
+        this.libraesterlina=libraesterlina
+
+    }
+    let.moneda=document.array.forEach(moneda=> {
+      [dolar, pesomexicano, pesocolombiano, euro, libraesterlina]
+    });('moneda').value;
+  Array.monedacapturar([dolar, pesomexicano, pesocolombiano, euro, libraesterlina]);
+    var dolarcapturar= document.getElementById("dolar").value;
+    var pesomexicanocapturar= document.getElementById("pesomexicano").value;
+    var pesocolombianocapturar= document.getElementById("pesocolombiano").value;
+    var eurocapturar= document.getElementById("euro").value;
+    var libraesterlinacapturar= document.getElementById("libraesterlina").value;
+     console.log(libraesterlinacapturar);
+
+ var nuevamoneda = new moneda(dolar, pesomexicano, pesocolombiano, euro, libraesterlina);
+}
+var basedatos=[];
+function capturar(){
+  console.log("capturar")
+}
+
+   
+    
+  
+
